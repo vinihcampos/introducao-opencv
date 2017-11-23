bd27260f7547e245ff26422ea924c153f68e7c6a
Vinícius Campos <viniciuscampos120@gmail.com>
2017-11-23 16:32:01 -0300

    Atualiza Aula02

--- Aula02/manipulando_pixels.py
+++ Aula02/manipulando_pixels.py
@@ -1,26 +1,47 @@
 # Importando biblioteca do OpenCV
 import cv2
 
-# Lendo imagens
-image = cv2.imread('../images/lena.png')
+# Lendo imagemns
+imagem = cv2.imread('../images/dory.jpg')
 
-print(image.shape)
+print(imagem.shape)
 
-# Obtendo as dimensões da imagem
-row,col,_ = image.shape
+# Obtendo as dimensões da imagemm
+row,col,_ = imagem.shape
 
 # Mostrando o valor do pixel na posição 0, 0
-(b, g, r) = image[0, 0]
+(b, g, r) = imagem[0, 0]
 print ("Pixel at (0, 0) - Red: %d, Green: %d, Blue: %d" % (r, g, b))
-		
 
-cv2.imshow('Imagem original', image)
+cv2.imshow('imagemm original', imagem)
 cv2.waitKey(0)
 
-# Alterando os valors dos pixels de um quarto da imagem
+imagem_original = imagem.copy();
+
+# Alterando os valors dos pixels de um quarto da imagemm
 for i in range (round(row/2)):
 	for j in range (round(col/2)):
-		image[i, j] = (255, 0, 0)
+		imagem[i, j] = (255, 0, 0)
 
-cv2.imshow('Imagem pintada', image)
+cv2.imshow('imagem pintada', imagem)
+cv2.waitKey(0)
+
+cv2.imshow('imagem original', imagem_original)
+cv2.waitKey(0)
+
+# Além dessa forma, temos:
+imagem = imagem_original.copy()
+imagem[0:round(row/2), 0:round(col/2) ] = (255, 0, 0)
+
+cv2.imshow('imagem pintada', imagem)
+cv2.waitKey(0)
+
+#Desenhando um círculo
+imagem = imagem_original.copy()
+
+centroX = round(col/2)
+centroY = round(row/2)
+
+cv2.circle(imagem, (centroX, centroY), centroY, (0, 255, 0) )
+cv2.imshow('imagem pintada', imagem)
 cv2.waitKey(0)
